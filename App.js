61599ef65dde9e23e1a81928060e8bc5df279f5a
Sourabh Shukla <82205268+anonymous372@users.noreply.github.com>
2022-03-19 09:47:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, SafeAreaView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, Button } from 'react-native';
+import { CurrentRenderContext, NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
@@ -8,14 +8,16 @@ const Stack = createNativeStackNavigator();
 
 
 function ExploreScreen ({navigation}) { 
+  // console.log("Explore")
   const [text, setText] = React.useState(null); 
   return (
-    <View>
-      <TextInput
+    <View style={styles}>
+      <Text>Explore</Text>
+      <TextInput 
         onChangeText={(text) => setText(text)}
       />
       <Button
-          title="Home"
+          style = {button}
           onPress={() => navigation.navigate('Home', {title: text})}
         />
   </View>
@@ -23,11 +25,13 @@ function ExploreScreen ({navigation}) {
 }
 
 function HomeScreen ({navigation, route}) {
+  console.log("Home",route)
+  const txt = route.params.title
   return (
-    <View>
-      <Text>{route.params.title}</Text>
-      <Button
-          title="Profile"
+    <View style={styles}>
+      <Text style={text}>{txt == null ? "Home Page" : txt}</Text>
+      <Button 
+          style = {button}
           onPress={() => navigation.navigate('Profile')}
         />
   </View>
@@ -37,7 +41,7 @@ function HomeScreen ({navigation, route}) {
 function ProfileScreen () {
   return (
     <View>
-      <Text>This is the Profile Page</Text>
+      <Text style={text}>This is the Profile Page</Text>
   </View>
   );
 }
@@ -45,7 +49,7 @@ function ProfileScreen () {
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Explore">
         <Stack.Screen name="Explore" component={ExploreScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
@@ -54,3 +58,25 @@ export default function App() {
   );
 } 
 
+const styles = {
+  marginTop : "5%",
+  // justifyContent: "center",
+  // alignItem : "center"
+}
+const text ={
+  fontSize: "25px",
+  textAlign: "center",
+  marginBottom: "20%"
+}
+const input = {
+  width: "70%",
+  margin: "5% 0 20%",
+  alignItems: "center",
+  padding: "10px 20px"
+}
+
+const button = {
+  width:"100px",
+  height: "40px",
+  padding: "10px 20px" 
+}
